66d940a27de1a246379dea0c770a8add1efcce75
Cindy Do <58800183+condoes@users.noreply.github.com>
2021-10-03 22:15:35 -0700

    Update cdo032_lab2_part1.c

--- turnin/cdo032_lab2_part1.c
+++ turnin/cdo032_lab2_part1.c
@@ -1,7 +1,7 @@
 /*	Author: Cindy Do
  *  Partner(s) Name: 
  *	Lab Section: 23
- *	Assignment: Lab #2  Exercise #
+ *	Assignment: Lab #2  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
